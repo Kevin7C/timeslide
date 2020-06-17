be34d8a81ef290ddafb7986c6e7930326f206965
Kevin7C <cnd245006350@163.com>
2020-06-17 16:08:12 +0800

    example更新

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -5,10 +5,24 @@ const appElement = document.getElementById('example');
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      value:[7,8,11,12]
+    }
+  }
+
+  handleClick=(value)=>{
+    this.setState({
+      value
+    })
+  }
   render() {
+    const {value}=this.state;
     return (
       <div>
-        <TimeSlide/>
+        <TimeSlide startTime={6}  endTime={22} />
       </div>
     );
   }
