3f2cbc78c2d0a5baba5990f748213eb2242b0aaa
Kevin7C <cnd245006350@163.com>
2020-06-17 16:08:53 +0800

    feature: timeslide update

--- src/timeslide.js
+++ src/timeslide.js
@@ -1,9 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import cx from 'classnames';
-// import css from './timeselect.module.scss'
-
-// import "./timeslide.css";
 
 const TimeSelectPadding = 10; //插件的左右padding
 const ValueWith = 60; //时间块宽
@@ -22,7 +18,7 @@ let timeitemcss = {
   cursor: "pointer",
 };
 let timeitemactive={
-    backgroundColor: "red",
+    backgroundColor: "#3091ff",
 }
 
 let labelcss = {
@@ -35,22 +31,22 @@ let labelcss = {
 export default class TimeSlide extends React.Component {
   constructor(props) {
     super(props);
+    const {startTime,endTime}=props;
+    const times=[];
+    const timeLabels=[];
+    for(let i=startTime;i<=endTime;i++){
+      times.push({
+        value:i,
+        isSelected:false
+      })
+    }
+    for(let i=startTime;i<=endTime+1;i++){
+      timeLabels.push(i);
+    }
     this.state = {
-      times: [
-        {
-          value: 1,
-          isSelected: false,
-        },
-        {
-          value: 2,
-          isSelected: false,
-        },
-        {
-          value: 3,
-          isSelected: false,
-        },
-      ],
-      timeLabels: [1, 2, 3, 4],
+      preValue:[],
+      times: times,
+      timeLabels: timeLabels,
     };
 
     this.refTimeSlide = React.createRef();
@@ -62,16 +58,47 @@ export default class TimeSlide extends React.Component {
       bottom: 0,
       width: 0,
       height: 0,
-      start:0,
-      end:0,
-      active:false
+      startIndex:0,
+      endIndex:0,
+      startPoint:{},
+      endPoint:{},
+      active:false,
+      abs:0
     };
+  }
+
+  static getDerivedStateFromProps(nextProps,state){
+    if("value" in nextProps && nextProps.value!==undefined&&!!state.times.length&&nextProps.value!==state.preValue){
+      
+      const {value}=nextProps;
+      const {times}=state;
+      for(let item of times){
+        if(value.includes(item.value)){
+          item.isSelected=true;
+        }else{
+          item.isSelected=false;
+        }
+      }
+      return {
+        ...state,
+        preValue:value
+      }
+    }
+
+    return state;
   }
 
   componentDidMount() {
     const dom = this.refTimeSlide.current.getBoundingClientRect();
     const { left, top, right, bottom, width, height } = dom;
     Object.assign(this.current, { left, top, right, bottom, width, height });
+  }
+
+  getCurrentValue=()=>{
+    let result=[]
+    const {times}=this.state;
+    result=times.filter(item=>item.isSelected).map(item=>item.value);
+    return result;
   }
 
   getIndex = (e) => {
@@ -82,29 +109,62 @@ export default class TimeSlide extends React.Component {
     return Math.floor(index);
   };
   handleMouseDown = (e) => {
+    const { clientX, clientY } = e;
     const {times}=this.state;
     const index=this.getIndex(e);
     this.current.isdown = true;
-    this.current.start=index
+    this.current.startIndex=index
+    this.current.startPoint={clientX, clientY};
     this.current.active=!times[index].isSelected;
   };
   handleMouseUp = (e) => {
+    const { clientX, clientY } = e;
     this.current.isdown = false;
-    this.current.end=this.getIndex(e);
+    this.current.endIndex=this.getIndex(e);
+    this.current.endPoint={clientX, clientY};
 
+    const {times}=this.state;
+    times[this.current.startIndex].isSelected=this.current.active;
+    this.setState({
+      times
+    },()=>{
+      const {onChange}=this.props;
+      if(!onChange){
+        return;
+      }
+      const result=this.getCurrentValue();
+      onChange(result);
+    })
   };
 
   handleMouseMove = (e) => {
-    if (this.current.isdown) {
+    let {isdown,active,startIndex,startPoint,abs}=this.current
+    if (isdown) {
+      const { clientX, clientY } = e;
       const {times}=this.state;
       const index=this.getIndex(e);
-      const selectedItem=times[index];
-      selectedItem.isSelected=this.current.active;
+      if(index<0){
+        return;
+      }
+
+      const currentAbs=Math.abs(clientX-startPoint.clientX);
+      let currentActive=active
+      if(currentAbs>=abs){
+        currentActive=active;
+      }else{
+        currentActive=!active;
+      }
+      this.current.abs=currentAbs;
+      times[index].isSelected=currentActive;
       this.setState({
         times
-      })
+      });
     }
   };
+
+  handleMouseLeave=(e)=>{
+    console.log("leave-out")
+  }
 
   render() {
     const { times, timeLabels } = this.state;
@@ -116,14 +176,15 @@ export default class TimeSlide extends React.Component {
           onMouseDown={this.handleMouseDown}
           onMouseMove={this.handleMouseMove}
           onMouseUp={this.handleMouseUp}
+          onMouseLeave={this.handleMouseLeave}
         >
           {times.map((item) => (
-            <span key={item.value} style={Object.assign({},timeitemcss,item.isSelected?timeitemactive:{})}></span>
+            <span key={`item${item.value}`} style={Object.assign({},timeitemcss,item.isSelected?timeitemactive:{})}></span>
           ))}
         </div>
         <p>
           {timeLabels.map((index) => (
-            <span key={index} style={labelcss}>
+            <span key={`label${index}`} style={labelcss}>
               {index}
             </span>
           ))}
@@ -134,13 +195,15 @@ export default class TimeSlide extends React.Component {
 }
 
 TimeSlide.propTypes = {
-  test: PropTypes.string,
-  disabled: PropTypes.bool,
-  startTime: PropTypes.string,
-  endTime: PropTypes.string,
+  value:PropTypes.oneOfType(PropTypes.array,null),
+  startTime: PropTypes.number,
+  endTime: PropTypes.number,
   onChange: PropTypes.func,
 };
 
 TimeSlide.defaultProps = {
-  test: "sfsdfd",
+  value:[],
+  startTime:6,
+  endTime:23,
+  onChange:()=>{}
 };
